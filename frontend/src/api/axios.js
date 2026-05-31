27ce511d2991134645caf79488e22d1f22998550
Ananya <g.ananyareddy02@gmail.com>
2026-06-01 00:05:39 +0530

    Fix production API URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    "http://localhost:5000/api",
+    import.meta.env.VITE_API_URL ||
+    "https://artisan-corner.onrender.com/api",
 });
 
 export default api;
